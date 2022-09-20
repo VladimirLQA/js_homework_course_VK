32a9711c39a3f774799c36e0e5d5467c65d2cc98
unknown <mr.kfpehtyrj@mail.ru>
2022-09-20 17:03:56 +0300

    fix

--- HW3_2.js
+++ HW3_2.js
@@ -2,8 +2,6 @@
 // которая будет выполнять эту работу.
 // Task 2 *** Реализуйте считывание из JSONки из файла task2.json с помощью, например, модуля fs.для дальнейшего использования в функции, описанной в задании
 
-
-
 const fs = require ("fs");
 let user = JSON.parse(fs.readFileSync("task2.json"));
 
@@ -13,7 +11,7 @@ function uniqueFunc() {
     user.forEach (el => {
         if(!unique1.includes(JSON.stringify(el)))  unique1.push(JSON.stringify(el))
     })
-    // return unique1.length;
+
     return unique1.map(el => JSON.parse(el))
 };
 console.log(uniqueFunc());
